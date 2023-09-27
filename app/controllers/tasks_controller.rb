80448b667efecd4fe90c6b7d61f204b9da806c9f
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-28 00:18:13 +0200

    Route new create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to days_path()
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content, :duration, :urgence, :importance, :activity)
+  end
 end
