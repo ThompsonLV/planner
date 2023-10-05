11b655ea5bda7e8be7829d1a3e4916dad0d76871
Thomas Le Veo <thomasleveo@gmail.com>
2023-10-05 12:10:05 +0200

    try Fullcalendar bundle

---
+++ app/javascript/controllers/add_event_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="add-event"
+export default class extends Controller {
+  connect() {
+    document.addEventListener('DOMContentLoaded', function() {
+      var calendarEl = document.getElementById('calendar');
+
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+          center: 'addEventButton'
+        },
+        customButtons: {
+          addEventButton: {
+            text: 'add event...',
+            click: function() {
+              var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+              var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+
+              if (!isNaN(date.valueOf())) { // valid?
+                calendar.addEvent({
+                  title: 'dynamic event',
+                  start: date,
+                  allDay: true
+                });
+                alert('Great. Now, update your database...');
+              } else {
+                alert('Invalid date.');
+              }
+            }
+          }
+        }
+      });
+
+      calendar.render();
+    });
+  }
+}
